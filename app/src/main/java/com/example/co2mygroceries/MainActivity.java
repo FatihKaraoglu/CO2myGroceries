1e84b1c45f0948123d7b03c3d3dfef548eda6970
KaraogluFatih <karaoglu.fatih16@gmail.com>
2021-11-26 12:56:18 +0100

    Initial commit

--- app/src/main/java/com/example/co2mygroceries/MainActivity.java
+++ app/src/main/java/com/example/co2mygroceries/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.co2mygroceries;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
+import com.example.co2mygroceries.data.ProductDbHelper;
+import com.example.co2mygroceries.data.FoodContracts;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ProductDbHelper mDbhelper = new ProductDbHelper(this);
+        SQLiteDatabase db = mDbhelper.getReadableDatabase();
     }
 }
---
+++ app/src/main/java/com/example/co2mygroceries/data/ProductDbHelper.java
@@ -0,0 +1,43 @@
+package com.example.co2mygroceries.data;
+
+
+import com.example.co2mygroceries.data.FoodContracts;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class ProductDbHelper extends SQLiteOpenHelper {
+     public static final Integer DATABASE_VERSION = 1;
+     public static final String DATABASE_NAME = "Product.db";
+     //Constructor
+     public ProductDbHelper (Context context) {
+         super(context, DATABASE_NAME, null, DATABASE_VERSION);
+     }
+         @Override
+         public void onCreate (SQLiteDatabase db){
+             String SQL_CREATE_ENTRIES = "CREATE TABLE " + DATABASE_NAME + "(" +
+                     FoodContracts.PRODUCT_ID + "INTEGER PRIMARY KEY AUTOINCREMENT," +
+                     FoodContracts.PRODUCT_CATEGORY + "TEXT," +
+                     FoodContracts.PRODUCT_NAME + "TEXT NOT NULL," +
+                     FoodContracts.PRODUCT_UNIT + "TEXT NOT NULL," +
+                     FoodContracts.PRODUCT_CO2E + "INTEGER NOT NULL, " +
+                     FoodContracts.PRODUCT_AGRICULTURE + " INTEGER," +
+                     FoodContracts.PRODUCT_iLUC + " INTEGER, " +
+                     FoodContracts.PRODUCT_FOODPROCESSING + " INTEGER ," +
+                     FoodContracts.PRODUCT_PACKAGING + " INTEGER, " +
+                     FoodContracts.PRODUCT_TRANSPORT + " INTEGER, " +
+                     FoodContracts.PRODUCT_RETAIL + " INTEGER);";
+             db.execSQL(SQL_CREATE_ENTRIES);
+         }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL(SQL_DELETE_ENTRIES);
+            onCreate(db);
+        }
+         @Override
+         public void onDowngrade (SQLiteDatabase db,int oldVersion, int newVersion){
+             onUpgrade(db, oldVersion, newVersion);
+         }
+     }
